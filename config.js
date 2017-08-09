110084f89d8088cddcb6386469dc7d936b751920
anna-marie-vos <amvos@outlook.com>
2017-08-09 12:08:02 +1200

    config changed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAJnD6FZAFZB8BAADGC0sVvQEK131wmJUsXD1J6bUdoVhfDVMkTcBvBLpuwV9Fc7mJVSqJtVCqxLcPe0Ma6ij6qldKt5xY41s6mnwnVhsF1PQZA3hCnlF6fctmZBjLkTyocNBOGGTGqzC6Eh7FZAazoRBqccTwGaRuAv736rsmQZDZD',
+    FB_VERIFY_TOKEN: 'herokuchatbot01',
+    API_AI_CLIENT_ACCESS_TOKEN: 'b83ef4c9d0474b7f8b0bad8a34c0de8b',
+    FB_APP_SECRET: '8b5989db1dc801153ce47ef547ae0612',
+    SERVER_URL: "https://herokuchatbotapp.herokuapp.com/",
 };
